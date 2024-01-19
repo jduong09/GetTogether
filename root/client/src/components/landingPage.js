e1861a29587a3e4b371423074bfc206ea4851db1
Justin Duong <jduong@umich.edu>
2024-01-19 16:25:51 -0500

    Finished carousel logic for steps list. '

--- root/client/src/components/landingPage.js
+++ root/client/src/components/landingPage.js
@@ -1,10 +1,45 @@
-import { React } from 'react';
+import { useState, useRef, React } from 'react';
 import '../css/landingPage.css';
 
 export const LandingPage = () => {
+  const [carouselIdx, setCarouselIdx] = useState(1);
+  const listStep1 = useRef(null);
+  const listStep2 = useRef(null);
+  const listStep3 = useRef(null);
+
   const handleNewPoll = (e) => {
     e.preventDefault();
     window.location = '/polls/new';
+  }
+
+  const handlePrevClick = (e) => {
+    e.preventDefault();
+    if (carouselIdx === 1) {
+      listStep1.current.classList.add('hide');
+      listStep3.current.classList.remove('hide');
+    } else if (carouselIdx === 2) {
+      listStep2.current.classList.add('hide');
+      listStep1.current.classList.remove('hide');
+    } else {
+      listStep3.current.classList.add('hide');
+      listStep2.current.classList.remove('hide');
+    }
+    setCarouselIdx(carouselIdx === 1 ? 3 : carouselIdx - 1);
+  }
+
+  const handleNextClick = (e) => {
+    e.preventDefault();
+    if (carouselIdx === 1) {
+      listStep1.current.classList.add('hide');
+      listStep2.current.classList.remove('hide');
+    } else if (carouselIdx === 2) {
+      listStep2.current.classList.add('hide');
+      listStep3.current.classList.remove('hide');
+    } else {
+      listStep3.current.classList.add('hide');
+      listStep1.current.classList.remove('hide');
+    }
+    setCarouselIdx(carouselIdx === 3 ? 1 : carouselIdx + 1);
   }
 
   return (
@@ -27,20 +62,26 @@ export const LandingPage = () => {
           </div>
         </div>
         <div id='div-steps'>
+          <button id='btn-prev-step' onClick={handlePrevClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+          </button>
           <ul id='list-workflow-steps'>
-            <li>
+            <li ref={listStep1}>
               <h2>Fill out the form</h2>
               <img id='img-form' src='/create_form.png' alt='UI for creating poll.' />
             </li>
-            <li>
+            <li ref={listStep2} className='hide'>
               <h2>Have peers respond</h2>
               <img id='img-response' src='/respond_form.png' alt='UI for responding to poll' />
             </li>
-            <li>
+            <li ref={listStep3} className='hide'>
               <h2>Find the ideal meeting time</h2>
               <img id='img-admin' src='/admin_page.png' alt='UI for seeing results' />
             </li>
           </ul>
+          <button id='btn-next-step' onClick={handleNextClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+          </button>
         </div>
       </main>
     </div>
